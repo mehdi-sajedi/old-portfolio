170e59f8f658df5ba272385fe3eb71032e2f9f08
mehdi-sajedi <mehdiisajedi@gmail.com>
2022-08-21 12:23:13 -0700

    Removed many unused and unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,9 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Mehdi Sajedi',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'Mehdi Sajedi is a Frontend Developer from San Diego, CA',
+    siteUrl: 'https://mehdisajedi.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'MehdiSajedi',
+        short_name: 'MehdiSajedi',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
